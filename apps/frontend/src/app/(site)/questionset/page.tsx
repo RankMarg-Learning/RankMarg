f1c9fc9b138cbc71fd17e4d9272925592a267714
AniketSudke <aniketsudke20@gmail.com>
2024-12-12 00:45:59 +0530

    -fix question-set

--- apps/frontend/src/app/(site)/questionset/page.tsx
+++ apps/frontend/src/app/(site)/questionset/page.tsx
@@ -11,8 +11,8 @@ import {
 } from "@/components/ui/table";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import SelectFilter from "@/components/SelectFilter";
-import React, {  useState } from "react";
-import { QTableRow } from "@/components/questions/QTableRow"; 
+import React, { useState } from "react";
+import { QTableRow } from "@/components/questions/QTableRow";
 import { QTableRowSkeleton } from "@/components/questions/QTableRowSkeleton";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
@@ -20,10 +20,14 @@ import { QuestionSetProps, QuestionTableProps } from "@/types";
 import Loading from "@/components/Loading";
 import RandomQuestion from "@/components/questions/RandomQuestion";
 import { Tags } from "@/constant/tags";
-import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
-
-
-
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
 const Questionset = () => {
   const [loading, setLoading] = useState(false);
@@ -31,53 +35,46 @@ const Questionset = () => {
   const [difficulty, setDifficulty] = useState("");
   const [tags, setTags] = useState("");
   const [search, setSearch] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
 
   const handleDifficulty = (value: string[]) => {
-    if(value[0] === "Default"){
+    if (value[0] === "Default") {
       setDifficulty("");
       return;
     }
     setDifficulty(value[0]);
   };
+
   const handleTags = (value: string[]) => {
-    if(value[0] === "Default"){
+    if (value[0] === "Default") {
       setTags("");
       return;
     }
     setTags(value[0]);
-  }
-
-
-  const [currentPage, setCurrentPage] = useState(1);
-  
-
+  };
 
   const { data } = useQuery({
-    queryKey: ["questions",currentPage],
+    queryKey: ["questions", currentPage],
     queryFn: async () => {
-      const response = await axios.get<QuestionSetProps>(`/api/question?page=${currentPage}`);
+      const response = await axios.get<QuestionSetProps>(
+        `/api/question?page=${currentPage}`
+      );
       const { data } = response;
       return data;
     },
-    
   });
 
-
-  
-    
-
-  const questions = data?.questionSet.filter((question:QuestionTableProps) => {
-    const matchesDifficulty = !difficulty || question.difficulty === difficulty;
-    const matchesTags = !tags || (question.tag && question.tag === tags);
-    const matchesSubject = !subject || question.subject === subject;
-    const matchesSearch = !search || 
-      question.content.toLowerCase().includes(search.toLowerCase()) || 
-      question.topic.toLowerCase().includes(search.toLowerCase());
-    return matchesDifficulty && matchesTags && matchesSubject && matchesSearch;
-  }) || [];
-
-  
-
+  const questions =
+    data?.questionSet.filter((question: QuestionTableProps) => {
+      const matchesDifficulty = !difficulty || question.difficulty === difficulty;
+      const matchesTags = !tags || (question.tag && question.tag === tags);
+      const matchesSubject = !subject || question.subject === subject;
+      const matchesSearch =
+        !search ||
+        question.content.toLowerCase().includes(search.toLowerCase()) ||
+        question.topic.toLowerCase().includes(search.toLowerCase());
+      return matchesDifficulty && matchesTags && matchesSubject && matchesSearch;
+    }) || [];
 
   const handlePageClick = (page: number) => {
     setCurrentPage(page);
@@ -93,171 +90,115 @@ const Questionset = () => {
     }
   };
 
-  
-    return (
-        (!loading ?(
-            <main className="flex flex-col  sm:gap-2 items-start gap-4 p-4 sm:px-3 sm:py-0 md:gap-8 ">
-              
-              <RandomQuestion 
-                setLoading={setLoading}
+  return !loading ? (
+    <div className="flex flex-col sm:gap-2 sm:py-4 mx-1">
+      <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+        <RandomQuestion setLoading={setLoading} />
+        <Tabs defaultValue="all">
+          <div className="grid gap-2 sm:flex sm:items-center">
+            <TabsList>
+              {["All", "Mathematics", "Physics", "Chemistry"].map((sub) => (
+                <TabsTrigger
+                  key={sub}
+                  value={sub}
+                  onClick={() => setSubject(sub === "All" ? "" : sub)}
+                >
+                  {sub}
+                </TabsTrigger>
+              ))}
+            </TabsList>
+            <div className="relative flex-1 my-2 sm:ml-7">
+              <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+              <Input
+                type="search"
+                placeholder="Search..."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                className="w-full rounded-lg bg-background pl-8 h-9"
               />
-              
-              <Tabs defaultValue="all">
-
-                <div className="grid md:flex items-center ">
-                  <TabsList>
-                    <TabsTrigger
-                      value="all"
-                      onClick={() => {
-                        setSubject("");
-                      }}
-                    >
-                      All
-                    </TabsTrigger>
-                    <TabsTrigger
-                      value="Mathematics"
-                      onClick={() => {
-                        setSubject("Mathematics");
-                      }}
-                    >
-                      Mathematics
-                    </TabsTrigger>
-                    <TabsTrigger
-                      value="Physics"
-                      onClick={() => {
-                        setSubject("Physics");
-                      }}
-                    >
-                      Physics
-                    </TabsTrigger>
-                    <TabsTrigger
-                      value="Chemistry"
-                      onClick={() => {
-                        setSubject("Chemistry");
-                      }}
-                    >
-                      Chemistry
-                    </TabsTrigger>
-                  </TabsList>
-                  <div className="relative md:ml-7  flex-1 md:mr-2 my-2  md:grow-0">
-                    <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                    <Input
-                      type="search"
-                      placeholder="Search..."
-                      value={search}
-                      onChange={(e) => setSearch(e.target.value)}
-                      className="w-full rounded-lg bg-background pl-8 h-9 md:w-[200px] lg:w-[320px]"
-                    />
-                  </div>
-                  <div className="mr-auto flex items-center gap-2">
-                    <SelectFilter
-                      width={"[100px]"}
-                      placeholder="Difficulty"
-                      selectName={["Default","Easy", "Medium", "Hard"]}
-                      onChange={handleDifficulty}
-                    />
-                    <SelectFilter
-                      width={"[100px]"}
-                      placeholder="PYQ"
-                      selectName={Tags}
-                      onChange={handleTags}
-                    />
-                    
-                  </div>
-                </div>
-                <TabsContent value={subject || "all"}>
-                  <Card x-chunk="dashboard-06-chunk-0" >
-                    <CardContent>
-                      <Table>
-                        <TableHeader>
-                          <TableRow>
-                            <TableHead className="hidden md:table-cell">Class</TableHead>
-                            <TableHead className="hidden md:table-cell">Difficulty</TableHead>
-                            <TableHead >Question </TableHead>
-                            <TableHead >Subject</TableHead>
-                            <TableHead className="hidden md:table-cell">Topic</TableHead>
-                            <TableHead className="hidden ">Accurcy</TableHead>
-                            
-                          </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                          {data?.questionSet 
-                            ? questions.map((question: QuestionTableProps) => {
-                                return (
-                                  
-                                  <QTableRow
-                                    key={question.id}
-                                    problem={question}
-                                  />
-                                );
-                              })
-                            : Array.from({ length: 20 }).map((_, index) => (
-                                <QTableRowSkeleton key={index} />
-                              ))}
-                        </TableBody>
-                      </Table>
-                    </CardContent>
-                    <CardFooter className="flex flex-1">
-                      
-                      <Pagination>
-                        <PaginationContent>
-                          <PaginationItem>
-                            <PaginationPrevious
-                              href="#"
-                              onClick={(e) => {
-                                e.preventDefault();
-                                handlePrevious();
-                              }}
-                              aria-disabled={currentPage === 1}
-                              className={currentPage === 1 ? "cursor-not-allowed opacity-50" : ""}
-                            />
-                          </PaginationItem>
-
-                          {Array.from({ length: data?.totalPages }, (_, i) => (
-                            <PaginationItem key={i + 1}>
-                              <PaginationLink
-                                href="#"
-                                onClick={(e) => {
-                                  e.preventDefault();
-                                  handlePageClick(i + 1);
-                                }}
-                                className={`${
-                                  i + 1 === currentPage
-                                    ? "bg-yellow-500 text-white font-semibold" 
-                                    : "bg-transparent text-muted-foreground"
-                                } p-2 rounded`}
-                              >
-                                {i + 1}
-                              </PaginationLink>
-                            </PaginationItem>
+            </div>
+            <div className="grid gap-2 sm:flex sm:items-center">
+              <SelectFilter
+                width={"[100px]"}
+                placeholder="Difficulty"
+                selectName={["Default", "Easy", "Medium", "Hard"]}
+                onChange={handleDifficulty}
+              />
+              <SelectFilter
+                width={"[100px]"}
+                placeholder="PYQ"
+                selectName={Tags}
+                onChange={handleTags}
+              />
+            </div>
+          </div>
+          <TabsContent value={subject || "all"}>
+            <Card>
+              <CardContent>
+                <div className="overflow-x-auto">
+                  <Table className="min-w-full">
+                    <TableHeader>
+                      <TableRow>
+                        {["Class", "Difficulty", "Question", "Subject", "Topic"].map(
+                          (head) => (
+                            <TableHead
+                              key={head}
+                              className="hidden md:table-cell"
+                            >
+                              {head}
+                            </TableHead>
+                          )
+                        )}
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      {questions.length
+                        ? questions.map((question) => (
+                            <QTableRow key={question.id} problem={question} />
+                          ))
+                        : Array.from({ length: 5 }).map((_, i) => (
+                            <QTableRowSkeleton key={i} />
                           ))}
+                    </TableBody>
+                  </Table>
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Pagination>
+                  <PaginationContent>
+                    <PaginationItem>
+                      <PaginationPrevious
+                        onClick={handlePrevious}
+                        aria-disabled={currentPage === 1}
+                      />
+                    </PaginationItem>
+                    {Array.from({ length: data?.totalPages || 1 }).map((_, i) => (
+                      <PaginationItem key={i}>
+                        <PaginationLink
+                          onClick={() => handlePageClick(i + 1)}
+                          className={currentPage === i + 1 ? "font-bold" : ""}
+                        >
+                          {i + 1}
+                        </PaginationLink>
+                      </PaginationItem>
+                    ))}
+                    <PaginationItem>
+                      <PaginationNext
+                        onClick={handleNext}
+                        aria-disabled={currentPage === data?.totalPages}
+                      />
+                    </PaginationItem>
+                  </PaginationContent>
+                </Pagination>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </main>
+    </div>
+  ) : (
+    <Loading />
+  );
+};
 
-                          <PaginationItem>
-                            <PaginationEllipsis />
-                          </PaginationItem>
-
-                          <PaginationItem>
-                            <PaginationNext
-                              href="#"
-                              onClick={(e) => {
-                                e.preventDefault();
-                                handleNext();
-                              }}
-                              aria-disabled={currentPage === data?.totalPages}
-                              className={currentPage === data?.totalPages ? "cursor-not-allowed opacity-50" : ""}
-                            />
-                          </PaginationItem>
-                        </PaginationContent>
-                      </Pagination>
-                    </CardFooter>
-                  </Card>
-                </TabsContent>
-              </Tabs>
-            </main>
-          ):(<Loading/> ))
-          
-  )
-}
-
-export default Questionset
-
+export default Questionset;
