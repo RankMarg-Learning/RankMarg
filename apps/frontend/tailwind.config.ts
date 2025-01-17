c62c962211be9a3e6e6e9518db4e4c2ecf38a4cf
AniketSudke <aniketsudke20@gmail.com>
2025-01-17 17:07:03 +0530

    Landing Page

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -72,9 +72,19 @@ const config: Config = {
   				to: {
   					height: '0'
   				}
-  			}
+  			},
+			'skew-scroll': {
+            '0%': {
+              transform: 'rotateX(20deg) rotateZ(-20deg) skewX(20deg)',
+            },
+            '100%': {
+              transform: 'rotateX(20deg) rotateZ(-20deg) skewX(20deg) translateY(-100%)',
+            },
+          },
+
   		},
   		animation: {
+			'skew-scroll': 'skew-scroll 20s linear infinite', //1
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
