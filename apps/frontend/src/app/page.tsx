0d5a5362e4b2a1114bab03702a7268132c1048ce
AniketSudke <aniketsudke20@gmail.com>
2025-01-28 12:57:54 +0530

    change landing-page

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -517,7 +517,7 @@ export default function Home() {
         {/* Qoute Section */}
         <section className="relative w-full h-52 sm:h-64 md:h-72 lg:h-80 xl:h-96 overflow-hidden flex justify-center items-center my-24 bg-yellow-900">
           <div className="absolute text-3xl sm:text-2xl md:text-6xl lg:text-7xl xl:text-8xl font-bold text-yellow-100 tracking-wide text-center px-4">
-            COMPETE.LEARN.ACHIEVE
+            LEARN.SOLVE.ACHIEVE
           </div>
         </section>
 
