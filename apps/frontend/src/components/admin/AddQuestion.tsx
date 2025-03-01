ccd277dfc1fea703f6953937d8d39222ad19781f
AniketSudke <aniketsudke20@gmail.com>
2025-03-01 13:50:28 +0530

    update question-data & test-response

--- apps/frontend/src/components/admin/AddQuestion.tsx
+++ apps/frontend/src/components/admin/AddQuestion.tsx
@@ -2,7 +2,7 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import React, { use, useEffect, useState } from "react";
+import React, {  useEffect, useState } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import SelectFilter from "@/components/SelectFilter";
 import { Button } from "@/components/ui/button";
