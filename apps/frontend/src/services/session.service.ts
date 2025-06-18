adcbcfc19a0f1e028a83f9a6fa44c834bf42aec7
AniketSudke <aniketsudke20@gmail.com>
2025-06-18 15:23:31 +0530

    api version set

--- apps/frontend/src/services/session.service.ts
+++ apps/frontend/src/services/session.service.ts
@@ -2,8 +2,9 @@ import api from "@/utils/api";
 
 // Get AI Practice Sessions API
 export const getAiPracticeSession = async (sessionId: string) => {
+    const version = process.env.VERSION || '/v.1.0'
     try {
-        const response = await api.get(`${process.env.VERSION}/ai-session/${sessionId}`);
+        const response = await api.get(`${version}/ai-session/${sessionId}`);
         return response.data;
     } catch (error) {
         console.error("Error fetching AI practice sessions:", error);
@@ -15,8 +16,9 @@ export const getAiPracticeSession = async (sessionId: string) => {
 }
 
 export const getAllAiPracticeSession = async (userId: string, sessionCount?: number) => {
+    const version = process.env.VERSION || '/v.1.0'
     try {
-        const response = await api.get(`${process.env.VERSION}/ai-practice?userId=${userId}&sessionCount=${sessionCount}`);
+        const response = await api.get(`${version}/ai-practice?userId=${userId}&sessionCount=${sessionCount}`);
         return response.data;
     } catch (error) {
         console.error("Error fetching AI practice sessions:", error);
