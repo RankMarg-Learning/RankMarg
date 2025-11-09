e564bdc5a9bc763d98e751bcec64b2678bc4125b
Aniket <aniketsudke3@gmail.com>
2025-11-09 20:52:10 +0530

    feat(mastery): add metadata for Subject Mastery page and implement navigation for topic practice button

--- apps/frontend/src/app/(site)/mastery/[subjectId]/page.tsx
+++ apps/frontend/src/app/(site)/mastery/[subjectId]/page.tsx
@@ -1,6 +1,21 @@
 import MasterySubjectPage from '@/components/mastery/MasterySubjectPage';
 import React from 'react'
 
+export const metadata = {
+  title: 'Subject Mastery | RankMarg',
+  description:
+    'Track your mastery progress in a specific subject with Rankmarg. Get detailed analytics, improvement areas, and smart recommendations to boost your performance. Elevate your preparation to the next level!',
+  openGraph: {
+    title: 'Subject Mastery for JEE & NEET – Rankmarg',
+    description:
+      'Track your mastery progress in a specific subject with Rankmarg. Get detailed analytics, improvement areas, and smart recommendations to boost your performance. Elevate your preparation to the next level!',
+    url: 'https://rankmarg.in/mastery',
+    type: 'website',
+  },
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
 
 const MasterySubject = ({ params }: { params: { subjectId: string } }) => {
     const { subjectId } = params
